fbfe8f454d43b132634138f3381818d03a4b03f8
Dragos Sabau <sabaudragos@yahoo.com>
2018-12-22 23:04:37 +0200

    Added header and organization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrganizationComponent } from './organization/organization.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+const appRoutes: Routes = [
+  {path: '', component: HomepageComponent },
+  {path: 'organization', component: OrganizationComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrganizationComponent,
+    HeaderComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
